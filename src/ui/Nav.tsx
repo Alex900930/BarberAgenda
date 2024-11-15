62b406f4a06679bef6efdd80f9680926678978dc
aherreramilet <aherrera@dl.cl>
2024-11-15 10:17:09 -0300

    modificado logo

--- src/ui/Nav.tsx
+++ src/ui/Nav.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import Logo1 from "../../public/assets/img/Logo1.png";
+import Image from 'next/image';
 
 export default function Nav() {
   return (
@@ -6,10 +8,15 @@ export default function Nav() {
       <nav className="bg-black text-white py-4 fixed w-full top-0 z-50">
         <div className="container mx-auto px-4 flex items-center justify-between">
           <div className="flex items-center space-x-2">
-            <svg viewBox="0 0 24 24" className="w-8 h-8 text-white" fill="none" stroke="currentColor">
+            {/* <svg viewBox="0 0 24 24" className="w-8 h-8 text-white" fill="none" stroke="currentColor">
               <path d="M7 8h10M7 12h10M7 16h10M3 4h18v16H3z" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
               <path d="M8 4v16M16 4v16" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>
+            </svg> */}
+            <Image
+                className="w-8 h-8 text-white"
+                src={Logo1}
+                alt='Logo'
+            />
             <span className="text-xl font-bold">Danilo&apos;s Barbearia Elite</span>
           </div>
           <div className="hidden md:flex space-x-6">
